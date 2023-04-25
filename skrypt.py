72f6405f0fcba3a373b2751bfb00d309e9cf6ac6
Zaneta002 <zanetajaniszek2@gmail.com>
2023-04-25 13:13:52 +0200

    tworzenie klasy

--- skrypt.py
+++ skrypt.py
@@ -5,3 +5,14 @@ Created on Mon Apr 24 21:46:34 2023
 @author: Zaneta
 """
 
+from math import *
+import numpy as np
+from funkcjeGW1 import * 
+
+class transformacje():
+    
+    def __init__(self              ):
+        """
+        Parametry elipssoid:
+            
+    
