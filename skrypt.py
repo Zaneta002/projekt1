3f6a605c32bd0bfda16d89ad529c4ad551a8e183
Zaneta002 <zanetajaniszek2@gmail.com>
2023-04-26 08:47:10 +0200

    poprawki do funkcji

--- skrypt.py
+++ skrypt.py
@@ -14,20 +14,20 @@ class Transformacje():
     
     def __init__(self, elipsoida: str ='GRS80'):
         """
-        Parametry elipsoid:
+        Parametry elipsoidy:
             a - duża półoś elipsoidy - promień równikowy
             b - mała półoś elipsoidy - promień południkowy
             flat - spłaszczenie
             e2 - mimośród^2
             
         """
-        self.parametry_elipsoid = {
+        self.parametry_elipsoidy = {
             "GRS80": (6378137.0, 6356752.31424518),
             "WGS84": (6378137.0, 6356752.31414036),
             "Krasowskiego": (6378245.0, 6356863.019),
         }
         try:
-            self.a, self.b = self.parametry_elipsoid[elipsoida]
+            self.a, self.b = self.parametry_elipsoidy[elipsoida]
         except KeyError:
             raise NotImplementedError(f"{elipsoida} elipsoida nie została zaimplementowana")
         
@@ -38,38 +38,97 @@ class Transformacje():
 
     
     
-    def hirvonen(self, X, Y, Z, a, e2):
+    def hirvonen(self, X, Y, Z, output = 'dec_degree'):
         """
-        DODAC DOKUMENTACJE 
+        Algorytm Hirvonena 
+        Funkcja przelicza współrzędne ortokartezjańskie (XYZ) na współrzędne geodezyjne (długość,
+        szerokość i wysokość elipsoidalna) - (phi, lambda, h). Jest to proces iteracyjny. 
+        W wyniku 3-4-krotneej iteracji wyznaczenia współrzędnej phi można przeliczyć współrzędne z dokładnoscią ok 1 cm.     
+        Dane:
+        ----------
+        X, Y, Z : FLOAT
+             współrzędne w układzie ortokartezjańskim, 
+
+        Zwraca:
+        -------
+        f (phi)
+            [stopnie dziesiętne] - szerokość geodezyjna
+        l (lambda)
+            [stopnie dziesiętne] - długośc geodezyjna
+        h : TYPE
+            [metry] - wysokość elipsoidalna
+            
+        output [STR] - opcjonalne, domyślne 
+            dec_degree - stopnie dziesiętne
+            dms - stopnie, minuty sekundy
         
         """
-    
         p = np.sqrt(X**2 + Y**2)        #promień równoleżnika 
         f = np.arctan(Z/(p*(1 - e2)))     #przybliżona wartosc fi
         while True:
-            N = a / np.sqrt(1-e2 * np.sin(f)**2)
+            N = self.a / np.sqrt(1 - self.e2 * np.sin(f)**2)
             h = (p/np.cos(f)) - N
             fpop = f
-            f = np.arctan(Z/(p*(1-(e2*N)/(N+h))))
+            f = np.arctan(Z / (p * (1 - (self.e2 * N) / (N + h))))
             if abs(fpop - f) < (0.000001/206265):
                 break
         l = np.arctan2(Y,X)
-        return(f,l,h) 
-    
-    
-    def flh2XYZ(self, f, l, h, a, e2):
-        """
-        DODAC DOKUMENTACJE
         
+        def dms(x):
+            sig = ''
+            if x < 0:
+                sig = '-'
+                x = abs(x)  
+            x = x * 180/pi
+            d = int(x)
+            m = int((x-d) * 60)
+            s = (x - d - m/60) * 3600
+            return(sig,"%3d %2d %7.5f" %(d,m,s))
+        
+        if output == "dec_degree":
+            return degrees(f), degrees(l), h 
+        elif output == "dms":
+            f = dms(f)
+            l = dms(l)
+            return f"{f[0]:02d}:{f[1]:02d}:{f[2]:.2f}", f"{l[0]:02d}:{l[1]:02d}:{l[2]:.2f}", f"{h:.3f}"
+        else:
+            raise NotImplementedError(f"{output} - jednostka wyjsciowa nie została zdefiniowana")
+            
+            
+#==========================================================================================================    
+    
+    
+    def flh2XYZ(self, f, l, h, output = 'metry'):
         """
-        N = a / np.sqrt(1-e2 * np.sin(f)**2)   #promień krzywizny w I wertykale
-        Xk = (N + h)*np.cos(f)*np.cos(l)
-        Yk = (N + h)*np.cos(f)*np.sin(l)
-        Zk = (N*(1-e2)+h)*np.sin(f)
+        Funkcja odwrotna do algorytmu Hirvonena. Przelicza współrzędne geodezyjne (phi, lambda, h) na 
+        współrzędne ortokartezjańskie (XYZ).
+        Dane:
+        ----------
+        f, l, h : FLOAT
+             współrzędne w układzie geodezyjnym
+
+        Zwraca:
+        -------
+        X : TYPE
+            [metry]
+      
+        Y : TYPE
+            [metry]
+            
+        Z: TYPE
+            [metry]
+           
+        output 
+            
+        """
+        N = self.a / np.sqrt(1 - self.e2 * np.sin(f)**2)   #promień krzywizny w I wertykale
+        Xk = (N + h) * np.cos(f) * np.cos(l)
+        Yk = (N + h) * np.cos(f )* np.sin(l)
+        Zk = (N * (1 - self.e2) +h) * np.sin(f)
         return(Xk, Yk, Zk)
     
    
-    
+#=====================================================================================================    
         
 # XYZ2neu to nie wiem czy git jest   
     
@@ -80,8 +139,8 @@ class Transformacje():
         """
 
         dX = np.array([X, Y, Z])
-        R = np.array([[-np.sin(f)*np.cos(l), -np.sin(l), np.cos(f)*np.cos(l)],
-                     [-np.sin(f)*np.sin(l), np.cos(l), np.cos(f)*np.sin(l)],
+        R = np.array([[-np.sin(f) * np.cos(l), -np.sin(l), np.cos(f) * np.cos(l)],
+                     [-np.sin(f) * np.sin(l), np.cos(l), np.cos(f) * np.sin(l)],
                      [np.cos(f), 0, np.sin(f)]])
         dneu = R.T @ dX
         s = np.sqrt(dneu @ dneu)
@@ -94,7 +153,7 @@ class Transformacje():
     def xyz_to_neu(self, X, Y, Z, f, l):
         f = radians(f)
         l = radians(l)
-        R = a / np.sqrt(1-e2 * np.sin(f)**2)
+        R = self.a / np.sqrt(1 - self.e2 * np.sin(f)**2)
         r = sqrt(X**2 + Y**2 + Z**2)
         f_g = asin(Z / r)    #geocentryczne fi i lambda punktu XYZ
         l_g = atan2(Y, X)
@@ -104,26 +163,26 @@ class Transformacje():
         return (N, E, U)
     
  
-    
+ #==================================================================================================   
  
 
-    def fl_to_uk2000(self, f, l, L0, n, a, e2):  #L0 - południk0 (15,18,21,24); n - strefa(5,6,7,8); nie wiem jak to ustawić
+    def fl_to_uk2000(self, f, l, L0, n):  #L0 - południk0 (15,18,21,24); n - strefa(5,6,7,8); nie wiem jak to ustawić
         """
         DOKUM
         
         """
-        b2 = (a**2)*(1-e2)
-        e22 = ((a**2) - b2) / b2
+        b2 = (self.a**2) * (1 - self.e2)
+        e22 = ((self.a**2) - b2) / b2
         dl = l - L0
         t = tan(f)
         eta2 = e22 * ((cos(f))**2)
-        N = a / np.sqrt(1-e2 * np.sin(f)**2)
+        N = self.a / np.sqrt(1 - self.e2 * np.sin(f)**2)
         
-        A0 = 1 - (e2/4) - (3 * e2**2/64) - (5 * e2**3/256)
-        A2 = 3/8 *((e2) + ( e2**2/4) + (15 * e2**3/128))
-        A4 = 15/256 *((e2**2) + (3 * e2**3/4))
-        A6 = 35 * e2**3/3072
-        sig = a * (A0 * f - A2 * sin(2*f) + A4 * sin(4*f) - A6 * sin(6*f))
+        A0 = 1 - (self.e2/4) - (3 * self.e2**2/64) - (5 * self.e2**3/256)
+        A2 = 3/8 *((self.e2) + (self.e2**2/4) + (15 * self.e2**3/128))
+        A4 = 15/256 * ((self.e2**2) + (3 * self.e2**3/4))
+        A6 = 35 * self.e2**3/3072
+        sig = self.a * (A0 * f - A2 * sin(2*f) + A4 * sin(4*f) - A6 * sin(6*f))
         
         Xgk = sig + ((dl**2)/2)*N*(sin(f))*(cos(f))*(1+((dl**2)/12)*((cos(f))**2)*(5-(t**2)+9*(eta2)+4*((eta2)**2))+((dl**4)/360)*((cos(f))**4)*(61-(58*(t**2))+(t**4)+(270*(eta2))-(330*(eta2)*(t**2))))                         
         Ygk = dl*N*(cos(f))*(1+((dl**2)/6)*((cos(f))**2)*(1-(t**2)+(eta2))+((dl**4)/120)*((cos(f))**4)*(5-(18*(t**2))+(t**4)+(14*eta2)-(58*(eta2)*(t**2))))
@@ -131,25 +190,28 @@ class Transformacje():
         X_2000 = Xgk * 0.999923
         Y_2000 = Ygk * 0.999923 + (n*1000000) + 500000
         return(X_2000, Y_2000)
+
+
+#===================================================================================================================
     
     
-    def fl_to_uk1992(self, f, l, L0, a, e2):  #nie wiem jak ustawić zeby L0 było zawsze 19*pi/180
+    def fl_to_uk1992(self, f, l, L0):  #nie wiem jak ustawić zeby L0 było zawsze 19*pi/180
         """
         DOKUM
         
         """
-        b2 = (a**2)*(1-e2)
-        e22 = ((a**2) - b2) / b2
+        b2 = (self.a**2) * (1 - self.e2)
+        e22 = ((self.a**2) - b2) / b2
         dl = l - L0
         t = tan(f)
         eta2 = e22 * ((cos(f))**2)
-        N = a / np.sqrt(1-e2 * np.sin(f)**2)
+        N = self.a / np.sqrt(1 - self.e2 * np.sin(f)**2)
         
-        A0 = 1 - (e2/4) - (3 * e2**2/64) - (5 * e2**3/256)
-        A2 = 3/8 *((e2) + ( e2**2/4) + (15 * e2**3/128))
-        A4 = 15/256 *((e2**2) + (3 * e2**3/4))
-        A6 = 35 * e2**3/3072
-        sig = a * (A0 * f - A2 * sin(2*f) + A4 * sin(4*f) - A6 * sin(6*f))
+        A0 = 1 - (self.e2/4) - (3 * self.e2**2/64) - (5 * self.e2**3/256)
+        A2 = 3/8 *((self.e2) + (self.e2**2/4) + (15 * self.e2**3/128))
+        A4 = 15/256 *((self.e2**2) + (3 * self.e2**3/4))
+        A6 = 35 * self.e2**3/3072
+        sig = self.a * (A0 * f - A2 * sin(2*f) + A4 * sin(4*f) - A6 * sin(6*f))
         
         Xgk = sig + ((dl**2)/2)*N*(sin(f))*(cos(f))*(1+((dl**2)/12)*((cos(f))**2)*(5-(t**2)+9*(eta2)+4*((eta2)**2))+((dl**4)/360)*((cos(f))**4)*(61-(58*(t**2))+(t**4)+(270*(eta2))-(330*(eta2)*(t**2))))                         
         Ygk = dl*N*(cos(f))*(1+((dl**2)/6)*((cos(f))**2)*(1-(t**2)+(eta2))+((dl**4)/120)*((cos(f))**4)*(5-(18*(t**2))+(t**4)+(14*eta2)-(58*(eta2)*(t**2))))
